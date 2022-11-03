8e052c6afcaedd95e47d17b4c5a0d2725f93b16b
gewalex <wgerve@gmail.com>
2022-11-03 18:39:41 -0400

    adding mutations and JWT

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,21 +3,21 @@ const { gql } = require('apollo-server-express');
 
 //  create our typeDfs
 const typeDefs = gql`
-type User {
-  _id: ID
-  username: String
-  email: String
-  friendCount: Int
-  thoughts: [Thought]
-  friends: [User]
-}
-type Thought {
+  type User {
     _id: ID
-    thoughtText: String
-    createdAt: String
     username: String
-    reactionCount: Int
-    reactions: [Reaction]
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+  type Thought {
+      _id: ID
+      thoughtText: String
+      createdAt: String
+      username: String
+      reactionCount: Int
+      reactions: [Reaction]
   }
   type Reaction {
     _id: ID
@@ -25,12 +25,25 @@ type Thought {
     createdAt: String
     username: String
   }
-    type Query {
-      users: [User]
-      user(username: String!): User
-      thoughts(username: String): [Thought]
-      thought(_id: ID!): Thought
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  addThought(thoughtText: String!): Thought
+  addReaction(thoughtId: ID!, reactionBody: String!): Thought
+  addFriend(friendId: ID!): User
+  } 
 `;
 
 // export the typDefs
